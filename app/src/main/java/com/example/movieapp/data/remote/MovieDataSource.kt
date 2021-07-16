b88d60ee8a7464dfd2746930f0659e1d17c2c412
cesaroer <cesarvarcard@gmail.com>
2021-07-15 23:24:52 -0500

    Added architecture mvvm and repository

---
+++ app/src/main/java/com/example/movieapp/data/remote/MovieDataSource.kt
@@ -0,0 +1,5 @@
+package com.example.movieapp.data.remote
+
+class MovieDataSource {
+
+}
--- app/src/main/java/com/example/movieapp/ui/movie_details/MovieDetailFragment.kt
+++ app/src/main/java/com/example/movieapp/ui/movie_details/MovieDetailFragment.kt
@@ -1,8 +1,9 @@
-package com.example.movieapp
+package com.example.movieapp.ui.movie_details
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import com.example.movieapp.R
 import com.example.movieapp.databinding.FragmentMovieDetailBinding
 
 class MovieDetailFragment : Fragment(R.layout.fragment_movie_detail) {
