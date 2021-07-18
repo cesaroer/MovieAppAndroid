d02676b08bad2fca02ea469171aecf96a483af05
cesaroer <cesarvarcard@gmail.com>
2021-07-18 00:47:30 -0500

    Added viewmodel lifecycle and live data

--- app/src/main/java/com/example/movieapp/presentation/MovieViewModel.kt
+++ app/src/main/java/com/example/movieapp/presentation/MovieViewModel.kt
@@ -1,4 +1,35 @@
 package com.example.movieapp.presentation
 
-class MovieViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.liveData
+import com.example.movieapp.core.Resource
+import com.example.movieapp.repository.MovieRespository
+import kotlinx.coroutines.Dispatchers
+import okhttp3.Dispatcher
+import java.lang.Exception
+
+class MovieViewModel(private val repo: MovieRespository): ViewModel() {
+
+    fun fetchUpcomingMovies() = liveData(Dispatchers.IO) {
+
+        emit(Resource.Loading())
+
+        try {
+            emit(Resource.Success(repo.getUpcomingMovies()))
+        }catch (e: Exception) {
+
+            emit(Resource.Failure(e))
+        }
+    }
+
+    class MovieViewModelFactory(private val repo: MovieRespository) : ViewModelProvider.Factory{
+        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+            return modelClass.getConstructor(MovieRespository::class.java).newInstance(repo)
+        }
+
+    }
+
+
+
 }
