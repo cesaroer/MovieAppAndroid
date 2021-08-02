e834c07d8185d1f5294cbb2b98e2d2abf85261ed
cesaroer <cesarvarcard@gmail.com>
2021-08-01 23:59:54 -0500

    Added filter to movies

--- app/src/main/java/com/example/movieapp/data/locale/LocaleMovieDataSource.kt
+++ app/src/main/java/com/example/movieapp/data/locale/LocaleMovieDataSource.kt
@@ -7,17 +7,17 @@ import com.example.movieapp.data.model.toMovieList
 class LocaleMovieDataSource(private val movieDao: MovieDao) {
     suspend fun getUpcomingMovies(): MovieList {
 
-        return movieDao.getAllMovies().toMovieList()
+        return movieDao.getAllMovies().filter { it.movie_type == "upcoming" }.toMovieList()
     }
 
     suspend fun getTopRatedMovies(): MovieList {
 
-        return movieDao.getAllMovies().toMovieList()
+        return movieDao.getAllMovies().filter { it.movie_type == "toprated" }.toMovieList()
     }
 
     suspend fun getPopularMovies(): MovieList {
 
-        return movieDao.getAllMovies().toMovieList()
+        return movieDao.getAllMovies().filter { it.movie_type == "popular" }.toMovieList()
     }
 
 }
