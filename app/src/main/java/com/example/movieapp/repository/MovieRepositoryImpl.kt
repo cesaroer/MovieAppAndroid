cfd5c053cc2ebf2db5fb061d1ad888b36e8c5b9a
cesaroer <cesarvarcard@gmail.com>
2021-08-17 00:37:58 -0500

    Check internet connection and save in DB results, if no internet return DB info

--- app/src/main/java/com/example/movieapp/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/com/example/movieapp/repository/MovieRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.example.movieapp.repository
 
 import android.util.Log
+import com.example.movieapp.core.InternetCheck
 import com.example.movieapp.data.locale.LocaleMovieDataSource
 import com.example.movieapp.data.model.MovieList
 import com.example.movieapp.data.model.toMovieEntity
@@ -12,41 +13,61 @@ class MovieRepositoryImpl(
 ) : MovieRepository {
 
     override suspend fun getUpcomingMovies(): MovieList {
-        dataSourceRemote.getUpcomingMovies().results.forEach { movie ->
 
-            Log.d("RetrievedMoviees", "Pelicula getUpcomingMovies recibida" + movie.toString())
+        return if (InternetCheck.isNetworkAvailable()) {
 
-            if(movie.backdrop_path != null){
-                dataSourceLocale.saveMovie(movie.toMovieEntity("upcoming"))
+            dataSourceRemote.getUpcomingMovies().results.forEach { movie ->
+
+                Log.d("RetrievedMoviees", "Pelicula getUpcomingMovies recibida" + movie.toString())
+
+                if (movie.backdrop_path != null) {
+                    dataSourceLocale.saveMovie(movie.toMovieEntity("upcoming"))
+                }
             }
+
+            dataSourceLocale.getUpcomingMovies()
+            //return dataSourceRemote.getUpcomingMovies()
+        } else {
+
+            dataSourceLocale.getUpcomingMovies()
         }
-
-
-        return dataSourceLocale.getUpcomingMovies()
-        //return dataSourceRemote.getUpcomingMovies()
     }
 
     override suspend fun getTopRatedMovies(): MovieList {
-        dataSourceRemote.getTopRatedMovies().results.forEach { movie ->
 
-            Log.d("RetrievedMoviees", "Pelicula getTopRatedMovies recibida" + movie.toString())
+        return if (InternetCheck.isNetworkAvailable()) {
 
-            dataSourceLocale.saveMovie(movie.toMovieEntity("toprated"))
+            dataSourceRemote.getTopRatedMovies().results.forEach { movie ->
+
+                Log.d("RetrievedMoviees", "Pelicula getTopRatedMovies recibida" + movie.toString())
+
+                dataSourceLocale.saveMovie(movie.toMovieEntity("toprated"))
+            }
+
+            dataSourceLocale.getTopRatedMovies()
+            //return dataSourceRemote.getTopRatedMovies()
+        } else {
+
+            dataSourceLocale.getTopRatedMovies()
         }
-
-        return dataSourceLocale.getTopRatedMovies()
-        //return dataSourceRemote.getTopRatedMovies()
     }
 
     override suspend fun getPopularMovies(): MovieList {
-        dataSourceRemote.getPopularMovies().results.forEach { movie ->
 
-            Log.d("RetrievedMoviees", "Pelicula getPopularMovies recibida" + movie.toString())
+        return if (InternetCheck.isNetworkAvailable()) {
 
-            dataSourceLocale.saveMovie(movie.toMovieEntity("popular"))
+            dataSourceRemote.getPopularMovies().results.forEach { movie ->
+
+                Log.d("RetrievedMoviees", "Pelicula getPopularMovies recibida" + movie.toString())
+
+                dataSourceLocale.saveMovie(movie.toMovieEntity("popular"))
+            }
+
+            dataSourceLocale.getPopularMovies()
+            //return dataSourceRemote.getPopularMovies()
+        } else {
+
+            dataSourceLocale.getPopularMovies()
         }
-
-        return dataSourceLocale.getPopularMovies()
-        //return dataSourceRemote.getPopularMovies()
     }
 }
